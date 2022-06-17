10dcc3c29c6bf71d59bd8f0c5a9d2681f287420f
zoubo <1051364728@qq.com>
2022-06-17 21:12:23 +0800

    完成了css和图片逻辑

--- src/main.js
+++ src/main.js
@@ -1,1 +1,17 @@
 import './app.vue'
+import './assets/fonts/iconfont.css'
+import gifStr from './assets/1.gif'
+import pngStr from './assets/logo_small.png'
+import $ from 'jquery'
+
+const gif = `<img src="${gifStr}"`
+const png = `<img src="${pngStr}"`
+
+$('body').append(gif)
+$('body').append(png)
+
+const fn = () => {
+    console.log('你好');
+}
+
+
